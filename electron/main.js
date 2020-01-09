c8198449a9b0a425d69eb6e606ad25e11d77ec4f
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2020-01-09 23:18:30 +0100

    adding progress bar example with ipc

--- electron/main.js
+++ electron/main.js
@@ -66,3 +66,15 @@ ipcMain.on('asynchronous-message', (event, arg) => {
     event.reply('asynchronous-reply', 'pong async')
   }, 1000)
 })
+ipcMain.on('test-progress-bar', (event, arg) => {
+  let progressVal = 0
+  const interval = setInterval(() => {
+    progressVal += 1
+    win.setProgressBar(progressVal / 100)
+    if (progressVal == 100) {
+      clearInterval(interval)
+      win.setProgressBar(-1)
+      event.reply('test-progress-bar-reply')
+    }
+  }, 100)
+})
