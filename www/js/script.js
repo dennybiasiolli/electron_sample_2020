27185a36ad760978f52367fb8cc20a667a51183c
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2020-01-09 22:59:15 +0100

    adding ipc example

--- www/js/script.js
+++ www/js/script.js
@@ -13,3 +13,21 @@ btnNotification.onclick = () => {
     console.log('Notification clicked')
   }
 }
+
+// ipc
+if (require) {
+  const { ipcRenderer } = require('electron')
+
+  const btnIpcSync = document.getElementById('btnIpcSync');
+  btnIpcSync.onclick = () => {
+    console.log(ipcRenderer.sendSync('synchronous-message', 'ping sync'))
+  }
+
+  ipcRenderer.on('asynchronous-reply', (event, arg) => {
+    console.log(arg)
+  })
+  const btnIpcAsync = document.getElementById('btnIpcAsync');
+  btnIpcAsync.onclick = () => {
+    ipcRenderer.send('asynchronous-message', 'ping async')
+  }
+}
