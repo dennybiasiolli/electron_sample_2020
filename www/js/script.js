19b8353d5df4602221b6cc1904a54d12c80f54b6
Denny Biasiolli <denny.biasiolli@gmail.com>
2020-01-09 22:51:09 +0100

    adding notification example

--- www/js/script.js
+++ www/js/script.js
@@ -2,3 +2,14 @@ var dynamicParagraph = document.getElementById('dynamicParagraph');
 setTimeout(function () {
   dynamicParagraph.innerHTML = '...it change after 2 seconds!';
 }, 2000);
+
+// notifications
+const btnNotification = document.getElementById('btnNotification');
+btnNotification.onclick = () => {
+  let myNotification = new Notification('Title', {
+    body: 'Lorem Ipsum Dolor Sit Amet'
+  })
+  myNotification.onclick = () => {
+    console.log('Notification clicked')
+  }
+}
